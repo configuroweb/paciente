d8e585116cc66858989b6a8c299ae55282db3222
Mauricio <msevillab@gmail.com>
2022-08-24 17:55:15 -0500

    primera versión funcional sistema de gestión de pacientes

---
+++ config/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer fixed-bottom">
+  <strong>Para más desarrollos <a href="https://www.configuroweb.com/46-aplicaciones-gratuitas-en-php-python-y-javascript/#Aplicaciones-gratuitas-en-PHP,-Python-y-Javascript">ConfiguroWeb</a></strong>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
